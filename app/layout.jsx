37f954f613a39d5a1510eed1e26ff0f984259b89
Prithvi <prithvin19@gmail.com>
2024-06-12 13:03:42 +0530

    m

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,8 +2,8 @@ import { Layout } from '@/components/dom/Layout'
 import '@/global.css'
 
 export const metadata = {
-  title: 'Next.js + Three.js',
-  description: 'A minimal starter for Nextjs + React-three-fiber and Threejs.',
+  title: 'Webibee-3D',
+  description: 'An imersive visual and sound UX experiment!',
 }
 
 export default function RootLayout({ children }) {
